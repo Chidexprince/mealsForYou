07e9c6700084883563e983294cc51643583db56d
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2021-11-16 01:59:09 +0100

    Setup Restaurant context and hooking the data to the screen

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import camelize from "camelize";
 
-import { mocks } from "./mock";
+import { mockImages, mocks } from "./mock";
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
       const mock = mocks[location];
@@ -14,12 +14,16 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
 
 export const restaurantsTransform = ({ results = [] }) => {
     const mappedResults = results.map((restaurant) => {
-        return{
+        restaurant.photos = restaurant.photos.map((p) => {
+            return mockImages[Math.ceil(Math.random() * (mockImages.length))]
+        })
+        return {
             ...restaurant,
+            address: restaurant.vicinity,
             isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
             isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
         };
-    })
+    });
 
     return camelize(mappedResults);
 };
